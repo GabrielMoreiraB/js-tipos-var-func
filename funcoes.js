403bacfff262f11fe18b645781e56ad1d7250fd8
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-01-17 19:31:51 -0300

    .

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+
+//imprimeTexto("oi");
+//imprimeTexto("outro texto");
+
+let s = 0;
+function soma (){
+    s =  2+2;
+    return s
+}
+
+console.log(soma())
